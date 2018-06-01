4230acc0da637241fd5a1024072e82458a39cbee
Edu Alcover <alcover.edu@gmail.com>
2018-06-01 14:26:28 +0200

    tareas añadidas en dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,43 @@
-alert('hola mundo')
+
+let createTask = (text) => `
+<div class="taskItem">
+${text}
+</div>
+`
+
+
+let addTask = function(e) {
+    if(e.keyCode===13){
+        let node = $(e.target);
+        let cont = node.parent().children('div');
+        //let cont = padre.children[1];
+        let input = node[0];
+        let taskText = input.value.trim();
+        if(taskText===''){
+            return ;
+        }
+        
+        let newTask = $(createTask(taskText));
+        $('.tareas').append(newTask);
+        
+        
+
+        localStorage.setItem('clave', taskText);
+        $('.text').val('');
+        
+    }
+}
+
+let deleteStore = () =>{
+    localStorage.clear();
+}
+
+
+let callBackOnReady = () =>{
+    $('.text').on('keypress', addTask);
+    $('.bot').on('click', deleteStore)
+}   
+
+
+
+$(document).ready(callBackOnReady);
