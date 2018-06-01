2a16196cce0ea6309070fd53fd0b472cefbdc552
Edu Alcover <alcover.edu@gmail.com>
2018-06-01 18:12:11 +0200

    proyecto acabado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,42 @@
 
-let createTask = (text) => `
-<div class="taskItem">
+let createTask = (text, id) => `
+<div class="taskItem" id="${id}">
 ${text}
 </div>
 `
 
+let deleteItemStorage = (e) =>{
+    let obj = $(e.target);
+    let id = obj[0].id;
+    let task = 'task-'+id;
+    obj.remove();
+    localStorage.removeItem(task);
+   }
 
+let i = 0;
+let id = 0;
 let addTask = function(e) {
     if(e.keyCode===13){
         let node = $(e.target);
-        let cont = node.parent().children('div');
-        //let cont = padre.children[1];
+        
         let input = node[0];
         let taskText = input.value.trim();
         if(taskText===''){
             return ;
         }
         
-        let newTask = $(createTask(taskText));
+        let newTask = $(createTask(taskText, id));
+        id++;
         $('.tareas').append(newTask);
-        
-        
 
-        localStorage.setItem('clave', taskText);
+
+        
+        
+            let task = 'task-'+i;
+            localStorage.setItem(task, taskText);
+          i++;  
         $('.text').val('');
+        $('.taskItem').on('click', deleteItemStorage);
         
     }
 }
@@ -33,9 +46,10 @@ let deleteStore = () =>{
 }
 
 
+
 let callBackOnReady = () =>{
     $('.text').on('keypress', addTask);
-    $('.bot').on('click', deleteStore)
+    $('.bot').on('click', deleteStore);
 }   
 
 
